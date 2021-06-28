c0d756753a098525523b48a6d9029dc4515cdd61
alexpeters1208 <80283343+alexpeters1208@users.noreply.github.com>
2021-06-28 14:50:27 -0600

    This is a working example of using the prototype framework to perform classification on the Iris dataset using PyTorch neural networks.

---
+++ iris_train_val_1.py
@@ -0,0 +1,236 @@
+# This example demonstrates a case where a user function creates partial tensors for each row.
+# These partial tensors are aggregated into tensors before evaluating the model.  
+# The aggregation should result in more efficient use of the AI machinery.  
+# The model function is then evaluated for each row to create results for the row.
+
+################################################################################################################################
+# Everything here would be part of a DH library
+################################################################################################################################
+
+from deephaven import QueryScope
+import jpy
+
+class Input:
+    def __init__(self, columns, gather):
+        if type(columns) is list: 
+            self.columns = columns
+        else:
+            self.columns = [columns]
+
+        self.gather = gather
+
+class Output:
+    def __init__(self, column, scatter, col_type="java.lang.Object"):
+        self.column = column
+        self.scatter = scatter
+        self.col_type = col_type
+
+def __gather_input(table, input):
+    #TODO: getDirect is probably terribly slow here, but it makes short code
+    data = [ table.getColumn(col).getDirect() for col in input.columns ]
+    return input.gather(*data)
+
+
+#TODO: clearly in production code there would need to be extensive testing of inputs and outputs (e.g. no null, correct size, ...)
+#TODO: ths is a static example, real time requires more work
+#TODO: this is not written in an efficient way.  it is written quickly to get something to look at
+
+# this handles input of length 1 and assumes user wants to use all columns
+def _default_input(inputs, table):
+    if len(inputs) > 1 and inputs[1] == "ALL":
+        # this creates a list of all columns in table that are not already inputs
+        remaining = list(table.dropColumns(inputs[0].columns[0]).getMeta().getColumn("Name").getDirect())
+        # create new list of inputs with all columns
+        new_inputs = [inputs[0]] + [Input(remaining, inputs[0].gather)]
+        return new_inputs
+    else:
+        return inputs
+
+
+def ai_eval(table=None, model_func=None, inputs=[], outputs=[]):
+    print("SETUP")
+    # append default inputs to inputs if needed
+    inputs = _default_input(inputs, table)
+
+    print("GATHER")
+    gathered = [ __gather_input(table, input) for input in inputs ]
+
+    # the following lines are done to reshape the data in a form the model will like, should not have to hard
+    # code this but the user should not have to make these transformations themselves
+    gathered[0] = torch.flatten(torch.transpose(gathered[0], 0, 1))
+    gathered[1] = torch.transpose(gathered[1], 0, 1)#.unsqueeze(0) adds extra dimension to beginning of tensor, may need
+
+    # if there are no outputs, we just want to call model_func and return nothing
+    if outputs == None:
+        print("COMPUTE NEW DATA")
+        model_func(*gathered)
+        return
+
+    else:
+        print("COMPUTE NEW DATA")
+        output_values = model_func(*gathered)
+
+        print(type(output_values))
+
+        print("POPULATE OUTPUT TABLE")
+        rst = table.by()
+        n = table.size()
+
+        for output in outputs:
+            print(f"GENERATING OUTPUT: {output.column}")
+            #TODO: maybe we can infer the type?
+            data = jpy.array(output.col_type, n)
+
+            #TODO: python looping is slow.  should avoid or numba it
+            for i in range(n):
+                data[i] = output.scatter(output_values, i)
+
+            QueryScope.addParam("__temp", data)
+            rst = rst.update(f"{output.column} = __temp")
+
+        return rst.ungroup()
+
+################################################################################################################################
+# Everything here would be user created -- or maybe part of a DH library if it is common functionality
+################################################################################################################################
+
+import random
+import numpy as np
+from deephaven.TableTools import readCsv
+from deephaven import QueryScope
+import torch
+import torchsummary
+from torch import Tensor
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import Softmax
+from torch.nn import Module
+from torch.optim import SGD
+from torch.nn import CrossEntropyLoss
+from torch.nn.init import kaiming_uniform_
+from torch.nn.init import xavier_uniform_
+from numpy import argmax
+from numpy import vstack
+from sklearn.metrics import accuracy_score
+
+# import data from sample data directory
+iris = readCsv("/data/examples/iris/csv/iris.csv")
+
+# since Class is categorical, we need to convert it to numeric
+# TODO: tihs is not great, a function to do all of this for me would be nice
+iris = iris.aj(iris.by("Class")\
+    .update("idx = i"), "Class", "idx")\
+    .dropColumns("Class")\
+    .renameColumns("Class = idx")
+
+# create model, this does not change with how you interact with ai_eval, so we put it at the top
+
+# model definition
+class MLP(Module):
+    # define model elements
+    def __init__(self, n_inputs):
+        super(MLP, self).__init__()
+        # input to first hidden layer
+        self.hidden1 = Linear(n_inputs, 10)
+        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+        self.act1 = ReLU()
+        # second hidden layer
+        self.hidden2 = Linear(10, 8)
+        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+        self.act2 = ReLU()
+        # third hidden layer and output
+        self.hidden3 = Linear(8, 3)
+        xavier_uniform_(self.hidden3.weight)
+        self.act3 = Softmax(dim=1)
+ 
+    # forward propagate input
+    def forward(self, X):
+        # input to first hidden layer
+        X = self.hidden1(X)
+        X = self.act1(X)
+        # second hidden layer
+        X = self.hidden2(X)
+        X = self.act2(X)
+        # output layer
+        X = self.hidden3(X)
+        X = self.act3(X)
+        return X
+
+
+# define model and set hyperparameters
+model = MLP(4)
+criterion = CrossEntropyLoss()
+optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+epochs=500
+batch_size = 20
+split = .75
+
+def train_and_validate(target, features):
+    # first, since we pass ai_eval one DH table, we must perform train/test split here
+    split_permutation = torch.randperm(features.size()[0])
+    num_train = round(features.size()[0] * split)
+    train_ind = split_permutation[0 : num_train - 1]
+    test_ind = split_permutation[num_train : features.size()[0] - 1]
+    train_target, train_features = target[train_ind], features[train_ind]
+    test_target, test_features = target[test_ind], features[test_ind]
+    # first, we train the model using the code from train_model given above.
+    # enumerate epochs, one loop represents one full pass through dataset
+    for epoch in range(epochs):
+        # create permutation for selecting mini batches
+        permutation = torch.randperm(train_features.size()[0])
+        # enumerate mini batches, one loop represents one batch for updating gradients and loss
+        for i in range(0, train_features.size()[0], batch_size):
+            # compute indices for this batch and split
+            indices = permutation[i:i+batch_size]
+            target_batch, features_batch = train_target[indices], train_features[indices]
+            # clear the gradients
+            optimizer.zero_grad()
+            # compute the model output
+            yhat = model(features_batch)
+            # calculate loss
+            loss = criterion(yhat, target_batch)
+            # credit assignment
+            loss.backward()
+            # update model weights
+            optimizer.step()
+    # print out a model summary using the torchsummary package
+    torchsummary.summary(model, (1,) + tuple(features.size()))
+
+    # now that we've trained the model, we perform validation on our test set, again using the code above
+    predictions, actuals = list(), list()
+    # evaluate the model on the test set
+    yhat = model(test_features)
+    # retrieve numpy array
+    yhat = yhat.detach().numpy()
+    actual = test_target.numpy()
+    # convert to class labels
+    yhat = argmax(yhat, axis=1)
+    # reshape for stacking
+    actual = actual.reshape((len(actual), 1))
+    yhat = yhat.reshape((len(yhat), 1))
+    # store
+    predictions.append(yhat)
+    actuals.append(actual)
+    predictions, actuals = vstack(predictions), vstack(actuals)
+    # calculate accuracy
+    acc = accuracy_score(actuals, predictions)
+    print("Accuracy score: " + str(acc))
+
+    predicted_classes = torch.argmax(model(features),1)
+
+    return predicted_classes
+
+
+def to_tensor(*data):
+    return torch.tensor(data)
+
+def to_scalar(data, i):
+    return int(data[i])
+
+
+ai_eval(table = iris, model_func = train_and_validate,
+    inputs = [Input("Class", to_tensor), Input(["SepalLengthCM","SepalWidthCM","PetalLengthCM","PetalWidthCM"], to_tensor)],
+    outputs = [Output("Predicted", to_table)])
